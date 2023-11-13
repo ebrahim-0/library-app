9cd8c607deed0b1bc678fa3a725dd415f82bd970
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-11-13 06:59:47 +0200

    split fetch

---
+++ components/ProductCard.jsx
@@ -0,0 +1,20 @@
+export default function ProductCard({ book }) {
+  return (
+    <div
+      key={Math.round(book._id * Math.random() * 1000) + i}
+      className="bg-white p-2 h-full grid rounded-md shadow-2xl"
+    >
+      <img
+        src={book.imageBook}
+        alt=""
+        className="w-full h-auto rounded-md object-cover"
+      />
+      <h3 className="my-7 text-xl text-center font-bold">{book.name}</h3>
+      <p className="text-slate-800">{book.description}</p>
+
+      <a href={book.pdfBook} download="your_file_name.pdf">
+        Download PDF
+      </a>
+    </div>
+  );
+}
--- components/Products.jsx
+++ components/Products.jsx
@@ -21,28 +21,19 @@ export default function Products() {
       <div className="w-4/5 mx-auto py-8">
         <h1 className="text-center text-4xl font-bold my-7">Our Books</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
-          {books.map((book, i) => (
-            <div
-              key={Math.round(book._id * Math.random() * 1000) + i}
-              className="bg-white p-2 h-full grid rounded-md shadow-2xl"
-            >
-              <img
-                src={book.imageBook}
-                alt=""
-                className="w-full h-auto rounded-md object-cover"
-              />
-              <h3 className="my-7 text-xl text-center font-bold">
-                {book.name}
-              </h3>
-              <p className="text-slate-800">{book.description}</p>
-
-              <a href={book.pdfBook} download="your_file_name.pdf">
-                Download PDF
-              </a>
-            </div>
-          ))}
+          <ProductCardList books={books} />
         </div>
       </div>
     </section>
   );
 }
+
+const ProductCardList = ({ books }) => {
+  return (
+    <>
+      {books.map((book, i) => (
+        <ProductCard book={book} />
+      ))}
+    </>
+  );
+};

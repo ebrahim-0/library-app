036537e99a535bda44280c9a26308a4b295a002a
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-11-15 02:26:36 +0200

    change revalidate to 0

--- app/api/book/route.js
+++ app/api/book/route.js
@@ -4,7 +4,8 @@ import { NextResponse } from "next/server";
 
 // export const dynamic = "force-dynamic";
 
-export const revalidate = 10;
+// export const revalidate = 10;
+export const revalidate = 0;
 
 export const GET = async () => {
   try {
